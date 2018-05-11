3c26655a47f3466addbdad46a4a495bdb05ed998
agonzalezgonzalez <agonzalezgonzalez@victoria4.danielcastelao.org>
2018-05-11 10:07:39 +0200

    Commit final

--- src/com/alexGonza/examen/Accion.java
+++ src/com/alexGonza/examen/Accion.java
@@ -7,12 +7,15 @@ package com.alexGonza.examen;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import javax.swing.JOptionPane;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
+import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
 
 @ActionID(
@@ -33,33 +36,51 @@ public final class Accion implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-       
-        String outdir=JOptionPane.showInputDialog("outdir");
-        String outfile=JOptionPane.showInputDialog("outfile");
-        String BCategory=JOptionPane.showInputDialog("BCategory");
-        String srcdir=JOptionPane.showInputDialog("srcdir");
-        String srcfiles=JOptionPane.showInputDialog("srcfiles");
-        String name=JOptionPane.showInputDialog("name");
-        String tittle=JOptionPane.showInputDialog("tittle");
-        String appclass=JOptionPane.showInputDialog("Appclass");
-        
-        try {
-            //comando a ejecutar en el terminal
-            String cmd = "javapackager -deploy -native deb"
-                    +" -Bcategory "+BCategory
-                    +" -outdir "+outdir
-                    +" -outfile "+outfile
-                    +" -srcdir "+srcdir
-                    +" -srcfiles "+srcfiles
-                    +" -appclass "+appclass
-                    +" -name "+name
-                    +" -title "+tittle;
-            //ejecuta en el terminal 
-            Runtime.getRuntime().exec(cmd);
-        } catch (IOException ioe) {
-            System.out.println(ioe);
-        }
-       
+        //pedimos los datos
+        String outdir = JOptionPane.showInputDialog("outdir");
+        String outfile = JOptionPane.showInputDialog("outfile");
+        String bcategory = JOptionPane.showInputDialog("category");
+        String srcdir = JOptionPane.showInputDialog("srcdir");
+        String srcfiles = JOptionPane.showInputDialog("srcfiles");
+        String appclass = JOptionPane.showInputDialog("appclass");
+        String nametitle = JOptionPane.showInputDialog("Titulo y nombre");
 
+       //Comando  que se ejecuta
+        
+                    String cmd = "javapackager -deploy -native deb "
+                    + "-Bcategory="+bcategory 
+                    + " -outdir " +outdir
+                    + " -outfile "+ outfile
+                    + " -srcdir "+srcdir
+                    + " -srcfiles "+srcfiles
+                    + " -appclass "+appclass
+                    + " -name "+nametitle
+                    + " -title "+nametitle;
+                    
+        try {
+            
+            //Ejecuta el comando en el terminal
+            
+            Runtime rt = Runtime.getRuntime();
+            
+            Process pr = rt.exec(cmd);
+            
+           
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+
+            String line = null;
+
+            while ((line = input.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitVal = pr.waitFor();
+            System.out.println("Exited with error code " + exitVal);
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        } catch (InterruptedException ex) {
+            Exceptions.printStackTrace(ex);
+        }
     }
 }
